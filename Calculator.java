097d6ded25a1eda0858d77b04a6ff99965fe4367
dayounglee <verdantjuly@kakao.com>
2024-01-18 08:11:29 +0900

    feat : java run practice

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+
+    public int subtract(int num1, int num2) {
+        int total;
+        total = num1 - num2;
+        return total;
+    }
+
+}
---
+++ Run.java
@@ -0,0 +1,7 @@
+public class Run {
+    public static void main(String args[]) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.subtract(3, 2));
+    }
+
+}
