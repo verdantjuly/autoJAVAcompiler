eab53687b3f294df661d7d4e6e9313994350566d
dayounglee <verdantjuly@kakao.com>
2024-01-18 12:18:45 +0900

    fix : compile시 하위 디렉토리 생기는 오류 해결

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,7 @@ package src;
 public class Calculator {
     public static void main(String[] args) {
         int num1 = 7;
-        int num2 = 2;
+        int num2 = 1;
         int total;
         total = num1 - num2;
         System.out.println(total);
